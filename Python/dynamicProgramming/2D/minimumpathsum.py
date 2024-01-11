7eeed04b5edede0677b2452bc5d3f99c24bdd82f
vaishu567 <rslv3vaishu567@gmail.com>
2024-01-11 14:58:14 +0530

    dp

--- Python/dynamicProgramming/2D/minimumpathsum.py
+++ Python/dynamicProgramming/2D/minimumpathsum.py
@@ -1,3 +1,25 @@
+def minsumpath(mat,right,down,row,col,n,m):
+    if row==n-1 and col==m-1:
+        return mat[row][col]
+    if col>m-1 or row>n-1:
+        return float('inf')
+    # movingright:
+    right=mat[row][col]+minsumpath(mat,right,down,row,col+1,n,m)
+    down=mat[row][col]+minsumpath(mat,right,down,row+1,col,n,m)
+    # print(right,down)
+    return min(right,down)
+mat=[[5,9,6],[11,5,2]]
+n=len(mat)
+m=len(mat[0])
+right=0
+down=0
+col=0
+row=0
+print(minsumpath(mat,right,down,row,col,n,m))
+
+
+
+# Memoization:
 def minsumpath(mat,right,down,row,col,n,m,dp):
     if row==n-1 and col==m-1:
         return mat[row][col]
