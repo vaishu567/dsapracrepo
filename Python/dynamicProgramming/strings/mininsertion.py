3bd19bd2359edd154e28839d78d402d9cb779514
vaishu567 <rslv3vaishu567@gmail.com>
2024-01-31 13:21:50 +0530

    dp

---
+++ Python/dynamicProgramming/strings/mininsertion.py
@@ -0,0 +1,21 @@
+
+# to solve this problem if we find out the longest palindrome subsequence in the str then we can subtract len of longest palindrome subsequence from len of actual str to get min insertions
+def minInsertion(s):
+    # Write the function here.
+    n=len(s)
+    s2=s[::-1]
+    dp=[[-1 for _ in range(n+1)] for _ in range(n+1)]
+    for i in range(n+1):
+        dp[i][0]=0
+    for j in range(n+1):
+        dp[0][j]=0
+    for i in range(1,n+1):
+        for j in range(1,n+1):
+            if s[i-1]==s2[j-1]:
+                dp[i][j]= 1+dp[i-1][j-1]
+            else:
+                dp[i][j]= 0+max(dp[i-1][j],dp[i][j-1])
+    # return dp
+    return n-dp[n][n]
+s="abca"
+print(minInsertion(s))
