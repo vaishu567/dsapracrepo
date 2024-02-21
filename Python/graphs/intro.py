4af55c2f44276976648d5d106420f94b7ecaad34
vaishu567 <rslv3vaishu567@gmail.com>
2024-02-21 17:34:38 +0530

    dsa

--- Python/graphs/intro.py
+++ Python/graphs/intro.py
@@ -16,22 +16,16 @@ print(inputgraph())
 #print(adjacency)
 
 
-def printAdjacency(n, m, edges):
-    # n=int(input("ennter no of  nodes in graph:"))
-    # m=int(input("ennter no of edges in graph:"))
-    adjacency=[[i] for i in range(n)]
-    for i in edges:
-        u,v=i[0],i[1]
-        adjacency[u].append(v)
-        adjacency[v].append(u)
-    return adjacency
-    
-# 3 2
-# 2 1
-# 2 0
-n=3
-m=2
-edges=[(2,1),(2,0)]
-adjacency=[[i] for i in range(n)]
-print(adjacency)
-print(printAdjacency(n, m, edges))
+# adjacency matrix:
+def inpmat():
+    n,m=map(int,input("Enter n and m values separated by a space: ").split())
+    mat=[[0  for i in range(m+1)] for  i in range(n+1)]
+    for _ in range(m):
+        u,v = map(int, input("Enter u and v values separated by a space: ").split())
+        mat[u][v]=1
+        mat[v][u]=1
+    return mat
+print(inpmat())
+
+
+
