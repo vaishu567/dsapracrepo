dd7819fbe3c63669563224dc76b72de3de8f33c6
vaishu567 <rslv3vaishu567@gmail.com>
2024-02-22 12:47:33 +0530

    dsa

--- Python/graphs/dfs.py
+++ Python/graphs/dfs.py
@@ -1,3 +1,5 @@
+# dfs using adjlist:
+
 def dfs(node,adj,vis,dfsa):
     vis[node]=1
     dfsa.append(node)
@@ -11,3 +13,27 @@ def dfsofgraph(n,adj):
     dfsa=[]
     dfsa=dfs(start,adj,vis,dfsa)
     return dfsa
+
+# dfs using adjMatrix:
+def dfs(node,adj,vis,dfsa):
+    vis[node]=1
+    dfsa.append(node)
+    for neighbour in adj[node]:
+        if vis[neighbour]!=1:
+            dfsa=dfs(neighbour,adj,vis,dfsa)
+    return dfsa
+def dfsadjmat(adjmat,n,m):
+    adjarr=[[]for i in range(n+1)]
+    for i in range(n):
+        for j in range(m+1):
+            if adjmat[i][j]==1:
+                adjarr[i].append(j)
+    vis=[0]*n
+    start=0
+    dfsa=[]
+    dfsa=dfs(start,adjarr,vis,dfsa)
+    return dfsa
+
+
+
+
