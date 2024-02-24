e499c5f0a0c9e8772df572bc36cd2dd1230de0cc
vaishu567 <rslv3vaishu567@gmail.com>
2024-02-24 17:14:04 +0530

    graph

--- Python/graphs/bfs+dfs/bipartite.py
+++ Python/graphs/bfs+dfs/bipartite.py
@@ -56,3 +56,31 @@ def isBipartite(graph):
 
 graph =[[1],[0,3],[3],[1,2]]
 print(isBipartite(graph))
+
+
+
+# bipartite dfs:
+def bipartitebooldfs(n,start,c,graph,color):
+    color[start]=c
+    for neighbour in graph[start]:
+        if color[neighbour]==-1:
+            if c==0:
+                if self.bipartitebooldfs(n,neighbour,1,graph,color)==False:
+                    return False
+            else:
+                if self.bipartitebooldfs(n,neighbour,0,graph,color)==False:
+                    return False
+        elif color[neighbour]==c:
+            return False
+    return True
+def isBipartite(graph):
+    n=len(graph)
+    color=[-1 for _ in range(n)]
+    for i in range(n):
+        if color[i]==-1:
+            if bipartitebooldfs(n,i,0,graph,color)==False:
+                return False
+    return True
+
+graph =[[1],[0,3],[3],[1,2]]
+print(isBipartite(graph))
--- Python/graphs/bfs+dfs/tempCodeRunnerFile.py
+++ Python/graphs/bfs+dfs/tempCodeRunnerFile.py
@@ -1,24 +1,26 @@
-
+def bipartitebooldfs(n,start,c,graph,color):
+    color[start]=c
+    for neighbour in graph[start]:
+        if color[neighbour]==-1:
+            if color[start]==0:
+                c=1
+            else:
+                c=0
+            bipartitebooldfs(n,neighbour,c,graph,color)
+        elif color[neighbour]==c:
+            return False
+    return True
 def isBipartite(graph):
     n=len(graph)
     color=[-1 for _ in range(n)]
-    to_visit=[0]
-    while to_visit:
-        node=to_visit.pop(0)
-        color[0]=0
-        for neighbour in graph[node]:
-            if color[neighbour]==-1:
-                to_visit.append(neighbour)
-                if color[node]==0:
-                    color[neighbour]=1
-                else:
-                    color[neighbour]=0
-            elif color[neighbour]==color[node]:
+    for i in range(n):
+        if color[i]==-1:
+            if bipartitebooldfs(n,i,0,graph,color)==False:
                 return False
-    for i in range(1,n):
-        if color[i]==color[i-1]:
-            return False
     return True
+
+
+
 
 graph =[[1],[0,3],[3],[1,2]]
 print(isBipartite(graph))
